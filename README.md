eeb85486c11b8565fd7c3bfaaf00118b2773a39d
Alexander.Skockiy <alexander.skockiy@scrdairy.com>
2023-01-18 17:22:12 +0200

    first

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # gui_repack_tgz
+## To assemble a new exe is needed install Python and PyInstaller
+### Download and install latest Python
+* [python.org/downloads](https://www.python.org/downloads/windows/)
+* [microsoft.com/store](https://apps.microsoft.com/store/search/python)
+### Enter PowerShell and install pyinstaller module
+```PowerShell
+pip install pyinstaller
+```
+### Assemble a new exe
+```PowerShell
+python -m PyInstaller --onefile --noconsole main.py
+```
+### Run dist/main.exe
---
+++ main.py
@@ -0,0 +1,39 @@
+import tempfile
+import shutil
+import tkinter as tk
+from tkinter import filedialog
+
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.title('Repack .tar.gz to .tar.gz')
+        self.geometry('500x75')
+
+        self.label = tk.Label(self, text='Chose .tar.gz archive')
+        self.label.pack()
+
+        self.open_button = tk.Button(self, text='Open', command=self.convert)
+        self.exit_button = tk.Button(self, text='Exit', command=self.exit)
+        self.open_button.pack(padx=15)
+        self.exit_button.pack(padx=15)
+
+    def convert(self):
+        fin = filedialog.askopenfilename(title='Select *.tar.gz archive', filetypes=[('Archives', '*.tar.gz')])
+        if fin:
+            with tempfile.TemporaryDirectory() as tempdir:
+                shutil.unpack_archive(fin, extract_dir=tempdir)
+
+                fout = filedialog.asksaveasfilename(title='Save as new_name archive', defaultextension="", filetypes=[('new_name', '')])
+                if fout:
+                    shutil.make_archive(fout, 'gztar', tempdir)
+                    self.label.config(text=f'File saved as {fout}.tar.gz')
+
+    def exit(self):
+        self.destroy()
+
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
